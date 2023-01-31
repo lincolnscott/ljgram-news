c61d954cb55eefb3c7af91c3bc23df344c96e3e0
Lincoln Scott <lincoln.jae.scott@gmail.com>
2023-01-31 13:33:30 -0600

    Rich Text Rendering

---
+++ app/(user)/post/[slug]/page.tsx
@@ -0,0 +1,97 @@
+import { groq } from 'next-sanity';
+import { client } from '../../../../lib/sanity.client';
+import Image from 'next/image';
+import urlFor from '../../../../lib/urlFor';
+import PortableText from 'react-portable-text';
+import { RichTextComponents } from '../../../../components/RichTextComponents';
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+async function Post({ params: { slug } }: Props) {
+  const query = groq`
+    *[_type == 'post' && slug.current == $slug][0] 
+    {
+      ...,
+      author->,
+      categories[]->,
+    }
+  `;
+
+  const post: Post = await client.fetch(query, { slug });
+
+  console.log(post);
+
+  return (
+    <article className='px-10 pb-28'>
+      <section className='space-y-2 border border-[#fcc40a] text-white'>
+        <div className='relative min-h-54 flex flex-col md:flex-row justify-between'>
+          <div className='absolute top-0 w-full h-full opacity-10 blur-sm p-10'>
+            {/* MAIN IMAGE AND AUTHOR NAME */}
+            <Image
+              className='object-cover object-center mx-auto'
+              src={urlFor(post.mainImage).url()}
+              alt={post.author.name}
+              fill
+            />
+          </div>
+          {/* STUFF IN BRAND COLOR IMAGE */}
+          <section className='p-5 bg-[#fcc40a] w-full'>
+            <div className='flex flex-col md:flex-row justify-between gap-y-5'>
+              <div>
+                {/* POST TITLE */}
+                <h1 className='text-4xl font-extrabold'>{post.title}</h1>
+
+                <p>
+                  {new Date(post._createdAt).toLocaleDateString('en-US', {
+                    day: 'numeric',
+                    month: 'long',
+                    year: 'numeric',
+                  })}
+                </p>
+              </div>
+
+              {/* AUTHOR IMAGE */}
+              <div className='flex items-center space-x-2'>
+                <Image
+                  className='rounded-full'
+                  src={urlFor(post.author.image).url()}
+                  alt={post.author.name}
+                  height={40}
+                  width={40}
+                />
+
+                {/* AUTHOR INFO */}
+                <div className='w-64'>
+                  <h3 className='text-lg font-bold'>{post.author.name}</h3>
+                  <div>{/* TODO: Author BIO */}</div>
+                </div>
+              </div>
+            </div>
+
+            {/* POST DESCRIPTION */}
+            <div>
+              <h2 className='italic pt-10'>{post.description}</h2>
+              <div className='flex items-center justify-end mt-auto space-x-2'>
+                {post.categories.map((category) => (
+                  <p
+                    key={category._id}
+                    className='bg-gray-800 text-white px-10 py-1 rounded-full text-sm font-semibold mt-4'>
+                    {category.title}
+                  </p>
+                ))}
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
+
+      <PortableText value={post.body} components={RichTextComponents} />
+    </article>
+  );
+}
+
+export default Post;
---
+++ components/Blogs.tsx
@@ -0,0 +1,67 @@
+import Image from 'next/image';
+import ClientSideRoute from './ClientSideRoute';
+import { ArrowUpRightIcon } from '@heroicons/react/24/solid';
+import urlFor from '../lib/urlFor';
+
+type Props = {
+  posts: Post[];
+};
+
+function Blogs({ posts }: Props) {
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24'>
+      {/* Posts */}
+      {posts.map((post) => (
+        <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
+          <div className='flex felx-col group cursor-pointer'>
+            {' '}
+            {/* image pointer hover */}
+            <div className='relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duraiton-200 ease-out'>
+              {' '}
+              {/* post on hover */}
+              <Image
+                className='object-cover object-left lg:object-center'
+                src={urlFor(post.mainImage).url()}
+                fill
+                alt={post.author.name}
+              />
+              <div className='absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between'>
+                <div>
+                  <p className='font-bold'>{post.title}</p>
+
+                  <p>
+                    {new Date(post._createdAt).toLocaleDateString('en-US', {
+                      day: 'numeric',
+                      month: 'long',
+                      year: 'numeric',
+                    })}
+                  </p>
+                </div>
+
+                <div className='flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center'>
+                  {post.categories.map((category) => (
+                    <div className='bg-[#fcc40a] text-center text-black px-3 py-1 rounded-full text-sm font-semibold'>
+                      <p>{category.title}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className='mt-5 flex-1'>
+              {' '}
+              {/* post title and description */}
+              <p className='underline text-lg font-bold'>{post.title}</p>
+              <p className='line-clamp-2 text-gray-500'>{post.description}</p>
+            </div>
+            <p className='font-bold flex items-center group-hover:underline'>
+              Read Post
+              <ArrowUpRightIcon className='ml-2 h-2 w-2' />
+            </p>
+          </div>
+        </ClientSideRoute>
+      ))}
+    </div>
+  );
+}
+
+export default Blogs;
