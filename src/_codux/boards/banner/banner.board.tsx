ca510fa1b46f870a67c5a1bd1cd33671d074d1af
Lincoln Scott <lincoln.jae.scott@gmail.com>
2023-02-06 13:35:16 -0600

    Remove Codux

--- src/_codux/boards/banner/banner.board.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createBoard } from '@wixc3/react-board';
-import Banner from '../../../../components/Banner';
-
-export default createBoard({
-    name: 'Banner',
-    Board: () => <Banner />
-});
--- src/_codux/boards/layout/root-layout.board.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { createBoard } from '@wixc3/react-board';
-import RootLayout from '../../../../app/(user)/layout';
-
-export default createBoard({
-    name: 'RootLayout',
-    Board: () => <RootLayout />
-});
